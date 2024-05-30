5bd72f1eb740e40ec47c3e655bbf151e2d35279e
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-30 15:21:19 -0700

    Add set_code function to CodeMaker class
    This function will ask for a code from the user
    or if it's a computer code maker, it will randomly
    generate one.

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,6 +1,22 @@
 class CodeMaker
+    attr_accessor :type, :code
     def initialize(type)
         @type = type
+        @code = []
     end
 
+    def set_code
+        if type == "human"
+            puts "Set your code"
+            print "---<Seperate each number with a space>---  "
+            self.code = gets.chomp.split(" ")
+        elsif type == "computer"
+            4.times do
+                self.code.push(rand(1..6)) 
+            end
+        end
+        return code
+    end
 end
+
+p CodeMaker.new("computer").set_code 
