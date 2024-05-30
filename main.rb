8e28567a00c5226bb57da475c8c304cfe53b10a5
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-30 12:08:34 -0700

    Add main.rb and psdeuocode

---
+++ main.rb
@@ -0,0 +1,15 @@
+#code breaker class
+#makes/ takes guesses
+#track number of tries
+#player type human or computer
+
+#code maker class
+#player type human or computer
+#set code 
+##randomly if computer
+#give feedback if computer
+
+#game logic class
+#set up using numbers 1-6 
+#can only have 4 slots for numbers
+#display guesses, feedback and tries taken
