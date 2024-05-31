a07ad9cbfa27ff9564721dcc9074bd2ddf61a8f2
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-05-31 12:01:25 -0700

    Add play function to game class
    This function sets up and controls the game loop
    between the code breaker and code maker.

--- lib/game.rb
+++ lib/game.rb
@@ -3,5 +3,17 @@ class Game
         @code_breaker = code_breaker
         @code_maker = code_maker
         @status = "lose"
+        @results = [0, 0] 
+    end
+
+    def play
+        p @code_maker.set_code #delete later
+        guess = @code_breaker.make_guess
+        @results = @code_maker.check_code(guess)
+        until @code_breaker.guesses == 0 || @results == [4, 0] 
+            puts "\nTry again... you have #{@code_breaker.guesses} turn(s) left"
+            guess = @code_breaker.make_guess
+            @results = @code_maker.check_code(guess)
+        end
     end
 end
