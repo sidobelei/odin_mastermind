3b77bfbe072272577a5eb42e461e90e0e44d660d
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-03 14:57:53 -0700

    Fix feedback bug for check_code function
    The check_code function was giving the incorrect
    number of colors that were of the color but wrong
    position. Changed some of the logic and added an
    extra function to help with code checking.
    Also changed some of the variable names for better
    clarity.

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -3,11 +3,11 @@ class CodeMaker
     
     include Validate
 
-    attr_accessor :type, :code
+    attr_accessor :type, :maker_code
 
     def initialize(type)
         @type = type
-        @code = []
+        @maker_code = []
     end
 
     def set_code
@@ -16,29 +16,59 @@ class CodeMaker
             if type == "human"
                 puts "Set your code. Valid inputs: r, o, y, g, b, p"
                 print "---<Seperate each letter with a space>--->  "
-                self.code = gets.chomp.split(" ")
+                self.maker_code = gets.chomp.split(" ")
             elsif type == "computer"
                 4.times do
                     color = rand(0..5)
-                    self.code.push(COLORS[color]) 
+                    self.maker_code.push(COLORS[color]) 
                 end
             end
-            valid_inputs = is_valid?(code)
+            valid_inputs = is_valid?(maker_code)
         end
-        return code
+        return maker_code
     end
 
-    def check_code(guess_code)
-        correct_position_value = 0 
-        code.each_with_index do |element, index|
-            if element == guess_code[index]
-                correct_position_value += 1
-            end 
+    def check_code(breaker_guess)
+        correct_position_value = 0
+        correct_value = 0
+        maker_code_positions = map_values_to_positions(maker_code)
+        breaker_code_positions = map_values_to_positions(breaker_guess)
+        maker_code_positions.each do |color, positions|
+            if breaker_code_positions.has_key?(color)
+                number_of_matches = 0
+                color_difference = 0
+                maker_total_color = maker_code_positions[color].length
+                breaker_total_color = breaker_code_positions[color].length
+
+                positions.each do |location|
+                    if breaker_code_positions[color].include?(location)
+                        number_of_matches += 1
+                    end
+                end
+
+                if maker_total_color > breaker_total_color
+                    color_difference = maker_total_color - breaker_total_color
+                elsif maker_total_color <= breaker_total_color 
+                    color_difference = maker_total_color
+                end
+                correct_value += color_difference - number_of_matches
+                correct_position_value += number_of_matches
+            end
         end
-        correct_value = 4 - (guess_code - code).length
-        correct_value -= correct_position_value
         puts "Colors that are in the right position: #{correct_position_value}"
         puts "Colors that are in the wrong position: #{correct_value}"
         return [correct_position_value, correct_value] 
+    end
+
+    def map_values_to_positions(code)
+        code_positions = {}
+        code.each_with_index do |element, index|
+            if code_positions.has_key?(element.to_sym)
+                code_positions[element.to_sym].push(index)
+            else
+                code_positions[element.to_sym] = [index]
+            end
+        end
+        return code_positions
     end
 end
