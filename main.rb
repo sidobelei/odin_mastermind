5f356776d2d2184a04c00e90d178a6fdd733e745
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-04 11:25:58 -0700

    Add game position selection functionality
    Users can choose to play either as the codebreaker
    or codemaker.

--- main.rb
+++ main.rb
@@ -2,10 +2,29 @@ require_relative "lib/code_breaker"
 require_relative "lib/code_maker"
 require_relative "lib/game"
 
-code_breaker = CodeBreaker.new("human")
-code_maker = CodeMaker.new("computer")
-game = Game.new(code_breaker, code_maker)
+answer = nil
+breaker_selected = nil
+maker_selected = nil
 
 puts "Welcome to Mastermind"
+print "Would you like to be the codebreaker or codemaker? "
+until ["codebreaker", "codemaker", "breaker", "maker"].include?(answer)
+    answer = gets.chomp.downcase.gsub(/\s+/, "")
+    if answer == "codebreaker" || answer == "breaker"
+        breaker_selected = "human"
+        maker_selected = "computer"
+    elsif answer == "codemaker" || answer == "maker"        
+        breaker_selected = "computer"
+        maker_selected = "human"
+    else
+        puts "\nInvalid Response"
+        print "Would you like to be the codebreaker or codemaker? "
+    end
+end
+
+code_breaker = CodeBreaker.new(breaker_selected)
+code_maker = CodeMaker.new(maker_selected)
+game = Game.new(code_breaker, code_maker)
+
 game.play
 game.check_results
