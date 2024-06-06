baa6465c0e69a7693cd5f204c128e0085e4f2752
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-06 13:18:35 -0700

    Add computer codebreaker functionality
    Added the computer_guesser and
    give_permutation_choice functions to the
    code_breaker class. The computer_guesser function
    will determine the colors in the code and the
    give_permutation_choice function helps with
    guessing their position. Also edited some other
    code in the game.rb and code_breaker.rb to
    accommodate a computer codebreaker.

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -3,22 +3,72 @@ class CodeBreaker
     
     include Validate
 
-    attr_accessor :guesses
+    attr_accessor :type, :guesses, :guess_history, :correct_colors_array, :correct_colors_permutations
 
     def initialize(type)
         @type = type
         @guesses = 12
+        @guess_history = []
+        @correct_colors_array = []
+        @correct_colors_permutations = []
     end
     
-    def make_guess
-        valid_inputs = false
-        until valid_inputs == true
-            print "---<Seperate each letter with a space>--->  "
-            maker_guess = gets.chomp.split(" ")
-            maker_guess.map! {|element| element.downcase}
-            valid_inputs = is_valid?(maker_guess) 
+    def make_guess(feedback = [0, 0])
+        maker_guess = []
+        if @type == "human"
+            valid_inputs = false
+            until valid_inputs == true
+                print "---<Seperate each letter with a space>--->  "
+                maker_guess = gets.chomp.split(" ")
+                maker_guess.map! {|element| element.downcase}
+                valid_inputs = is_valid?(maker_guess) 
+            end
+        else 
+            maker_guess = computer_guesser(feedback)
         end
         self.guesses -= 1
         return maker_guess
+    end
+
+    def computer_guesser(feedback)
+        computer_guess = []
+        attempt = 12 - guesses
+        total_correct_colors = feedback.sum
+        if correct_colors_array.empty?
+            if attempt == 0
+                4.times do computer_guess.push(COLORS[attempt])
+                end
+            elsif total_correct_colors != 4 
+                guess_history[attempt - 1].each_with_index do |color, position|
+                    if position < total_correct_colors
+                        computer_guess.push(guess_history[attempt - 1][position])
+                    else
+                        computer_guess.push(COLORS[attempt])
+                    end
+                end
+            else
+                self.correct_colors_array = guess_history[attempt - 1]
+                computer_guess = guess_history[attempt - 1]
+            end
+        end
+        if !correct_colors_array.empty?
+            computer_guess = give_permutation_choice(guess_history[attempt - 1])
+        end
+        self.guess_history.push(computer_guess)
+        print "---<Seperate each letter with a space>---> "
+        computer_guess.each {|color| print "#{color} "}
+        puts "\n"
+        return computer_guess
+    end
+    
+    def give_permutation_choice(array)
+        if correct_colors_permutations.empty?
+            self.correct_colors_permutations = array.permutation(4).to_a.uniq
+            self.correct_colors_permutations.delete(array)
+        end
+        random_index = rand(0..(correct_colors_permutations.length - 1))
+        permutation_choice = correct_colors_permutations[random_index]
+        self.correct_colors_permutations.delete(permutation_choice)
+        return permutation_choice
     end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,7 @@ class Game
         @results = @code_maker.check_code(guess)
         until @code_breaker.guesses == 0 || @results == [4, 0] 
             puts "\nTry again... you have #{@code_breaker.guesses} turn(s) left"
-            guess = @code_breaker.make_guess
+            guess = @code_breaker.make_guess(@results)
             @results = @code_maker.check_code(guess)
         end
     end
@@ -21,6 +21,14 @@ class Game
         if @results == [4, 0]
             @status = "win"
         end
-        puts "\nYou #{@status}!"
+        if @code_breaker.type == "human"
+            puts "\nYou #{@status}!"
+        else
+            if @status == "win"
+                puts "\nThe Computer cracked the code, you lose!"
+            else
+                puts "\nThe Computer couldn't crack the code, you win!"
+            end
+        end
     end
 end
